f2f03b45e7b4dcbac4565adf5c2941664d2e6bf8
rayenebech <rayenebech@gmail.com>
2026-05-21 21:41:10 +0300

    Fix solution to ex 2.20

--- part2/countries/src/services/weather.js
+++ part2/countries/src/services/weather.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const baseUrl = 'https://api.openweathermap.org/data/3.0/onecall'
-const api_key = import.meta.env.VITE_SOME_KEY
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather'
+const api_key = import.meta.env.VITE_WEATHER_API_KEY
 
 const getWeather = (lat, lon) => {
   const get_url = `${baseUrl}?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`
