352cdbe61a6186520317a4d7a69156abf3e0b3ea
rayenebech <rayenebech@gmail.com>
2026-05-21 21:34:06 +0300

    Add solutions to exercises 3.1 to 3.6

---
+++ part3/phonebook/index.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+const generateId = () => {
+  const maxId = Math.floor(Math.random() * 1000);
+  return String(maxId)
+}
+
+let contacts = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (request, response) => {
+  let numberOfContacts = contacts.length
+  const info = `<p>Phonebook has info for ${numberOfContacts} people</p><p>${new Date()}</p>`
+  response.send(info)
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(contacts)
+})
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const contact = contacts.find(contact => contact.id === id)
+   if (contact) {
+    response.json(contact)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'name or number missing' 
+    })
+  }
+    if (contacts.find(contact => contact.name.toLowerCase() === body.name.toLowerCase())) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+
+  const contact = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  contacts = contacts.concat(contact)
+
+  response.json(contact)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  contacts = contacts.filter(contact => contact.id !== id)
+
+  response.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
