ee85f839fd6e057847dcd62a9be4e9a411c75899
rayenebech <rayenebech@gmail.com>
2026-05-24 11:40:42 +0300

    Remove cors from the backend.

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 
 
 morgan.token('body', (req) => req.body ? JSON.stringify(req.body) : '')
@@ -8,7 +7,6 @@ morgan.token('body', (req) => req.body ? JSON.stringify(req.body) : '')
 const app = express()
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-app.use(cors())
 app.use(express.static('dist'))
 
 
