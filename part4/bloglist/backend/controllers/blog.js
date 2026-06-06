f75cccefebee9857ed536d30d73da914ece9e1ec
rayenebech <rayenebech@gmail.com>
2026-06-07 00:02:06 +0300

    Add delete and update endpoints.

--- part4/bloglist/backend/controllers/blog.js
+++ part4/bloglist/backend/controllers/blog.js
@@ -6,10 +6,32 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  response.json(blog)
+})
+
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
   const savedBlog = await blog.save()
   response.status(201).json(savedBlog)
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const { title, author, url, likes } = request.body
+  const blogToUpdate = await Blog.findById(request.params.id)
+  blogToUpdate.title = title
+  blogToUpdate.author = author
+  blogToUpdate.url = url
+  blogToUpdate.likes = likes
+  await blogToUpdate.save()
+  response.json(blogToUpdate)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const blogToDelete = await Blog.findById(request.params.id)
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = blogsRouter
