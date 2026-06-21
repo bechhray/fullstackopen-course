21204f10a10389a81abb33f6ee2b386ec6435bdd
rayenebech <rayenebech@gmail.com>
2026-06-21 16:25:37 +0300

    Add solution to 5.17

---
+++ part5/bloglist/frontend/tests/blog_app.spec.js
@@ -0,0 +1,15 @@
+
+import { test, expect } from '@playwright/test'
+
+test.describe('Blog app', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    const loginForm = page.getByText('log in to application')
+    await expect(loginForm).toBeVisible()
+  })
+})
+
+
